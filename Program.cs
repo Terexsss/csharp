c9914d9602847b041a7a9d6cba330f514b667634
Your Name <you@example.com>
2024-08-26 22:03:53 +0700

    уравнения

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+
+Console.WriteLine(sum);
+
+
+double d1 = 5.5;
+long l1 = 6;
+
+int product1 = i1*i2;
+long product2 = l1 * i2;
+double product3 = d1 * l1;
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
